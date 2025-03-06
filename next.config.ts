f008637db8b5668b387c3b97fe96f4f40cb20569
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-03-06 05:46:15 +0200

    add github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/portfolio_frontend',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
