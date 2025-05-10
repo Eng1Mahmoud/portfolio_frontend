a43cf1a33b354ce093007a7bfa30a317eedeacea
Mahmoud Mohamed <mahmoudabbamalik@gmail.com>
2025-05-10 19:21:27 +0300

    Feat/google analytics (#6)
    * feat(analytics): integrate Google Analytics and track CV downloads
    
    Add Google Analytics integration using @next/third-parties to track user interactions, specifically CV downloads. This helps in understanding user engagement and behavior on the portfolio site.
    
    * fix: correct typo in GA event name for CV download
    
    The event name "dounload_cv" was misspelled. This commit corrects it to "download_cv" to ensure accurate tracking of CV download events in Google Analytics.
    
    * Centralize the  event tracking logic for CV download by creating a reusable function in `events.ts`. This function is now imported and used in both `PersonalInfo.tsx` and `Aside.tsx` to ensure consistent tracking across the application.
    
    * This commit adds "dev-mahmoud.sirv.com" to the list of allowed image domains in the Next.js configuration. This change is necessary to ensure images hosted on this domain are properly loaded in the application.
    
    * fix(next.config.ts): add missing wildcard pattern for image hostname
    
    The wildcard pattern was missing in the image hostname configuration, which could cause issues with loading images from subpaths. This change ensures that all subpaths under the specified hostname are correctly handled.
    
    * refactor(next.config.ts): simplify image domains configuration
    
    Consolidate image domains configuration by removing redundant entry and using pathname pattern to match all subpaths under the specified hostname. This improves maintainability and reduces unnecessary duplication.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "https",
+        hostname: "dev-mahmoud.sirv.com",
+        pathname: "**",
+      },
     ],
   },
 };
