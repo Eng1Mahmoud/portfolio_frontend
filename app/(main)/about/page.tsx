5845a6da7f03634f4e52d9cdf59922e81e6d5f1e
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-05-15 16:11:23 +0300

    feat(seo): add structured data markup for about page and layout
    Add JSON-LD structured data using `schema-dts` library to enhance SEO for the about page and root layout. This improves search engine understanding of the content and increases visibility in search results.

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -4,11 +4,34 @@ import { ProfileImage } from "@/components/about/ProfileImage";
 import { Title } from "@/components/general/Title";
 import { IuserInfo } from "@/types/general";
 import { Metadata } from "next";
+import { AboutPage, WithContext } from "schema-dts";
+// metadata
 export const metadata: Metadata = {
   title: "About Me",
   description: "About Mahmoud Mohamed",
 };
-export default async function AboutPage() {
+// JSONLD
+const jsonld: WithContext<AboutPage> = {
+  "@context": "https://schema.org",
+  "@type": "AboutPage",
+  name: "About Me",
+  description:
+    "I am  Frontend Engineer with a degree in Computers and Artificial Intelligence, plus one year of startup experience. Specializing in React and Next.js, I create responsive, high-performance web applications that drive user engagement and business growth. what I offer: - Expertise in React, Next.js, and modern front-end technologies - Rapid development of user-friendly, scalable web solutions - Ability to balance technical excellence with business objectives - Collaborative approach, thriving in fast-paced environments Committed to delivering innovative web experiences that exceed expectations and propel digital success.",
+  url: "https://dev-mahmoud-portfolio.vercel.app/", // Replace with your actual URL
+  author: {
+    "@type": "Person",
+    name: "Mahmoud Mohamed",
+    url: "https://dev-mahmoud-portfolio.vercel.app/",
+  },
+  mainEntity: {
+    "@type": "Person",
+    name: "Mahmoud Mohamed",
+    description:
+      "I am  Frontend Engineer with a degree in Computers and Artificial Intelligence, plus one year of startup experience. Specializing in React and Next.js, I create responsive, high-performance web applications that drive user engagement and business growth. what I offer: - Expertise in React, Next.js, and modern front-end technologies - Rapid development of user-friendly, scalable web solutions - Ability to balance technical excellence with business objectives - Collaborative approach, thriving in fast-paced environments Committed to delivering innovative web experiences that exceed expectations and propel digital success.",
+    url: "https://dev-mahmoud-portfolio.vercel.app/",
+  },
+};
+export default async function AboutUsPage() {
   const profileInfo = await getProfileInfo();
   return (
     <div>
@@ -20,6 +43,12 @@ export default async function AboutPage() {
         {/* Personal Information */}
         <PersonalInfo profileInfo={profileInfo as IuserInfo} />
       </div>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonld),
+        }}
+      />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { mainFont } from "@/fonts";
 import { Metadata } from "next";
-
+import { Person, WithContext } from "schema-dts";
+// metadata
 export const metadata: Metadata = {
   title: {
     default: "Mahmoud Mohamed | Portfolio",
@@ -20,7 +21,20 @@ export const metadata: Metadata = {
     "next.js",
   ],
 };
-
+// JSONLD
+const jsonld: WithContext<Person> = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Mahmoud Mohamed",
+  url: "https://dev-mahmoud-portfolio.vercel.app/",
+  sameAs: [
+    "https://github.com/Eng1Mahmoud",
+    "https://www.linkedin.com/in/Mahmoud-Mohamed-Abdel-Aal",
+  ],
+  jobTitle: "Frontend Engineer",
+  description:
+    "I am  Frontend Engineer with a degree in Computers and Artificial Intelligence, plus one year of startup experience. Specializing in React and Next.js, I create responsive, high-performance web applications that drive user engagement and business growth. what I offer: - Expertise in React, Next.js, and modern front-end technologies - Rapid development of user-friendly, scalable web solutions - Ability to balance technical excellence with business objectives - Collaborative approach, thriving in fast-paced environments Committed to delivering innovative web experiences that exceed expectations and propel digital success.",
+};
 export default async function RootLayout({
   children,
 }: {
@@ -32,19 +46,7 @@ export default async function RootLayout({
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Person",
-              name: "Mahmoud Mohamed",
-              url: "https://dev-mahmoud-portfolio.vercel.app/",
-              sameAs: [
-                "https://github.com/Eng1Mahmoud",
-                "https://www.linkedin.com/in/Mahmoud-Mohamed-Abdel-Aal",
-              ],
-              jobTitle: "Frontend Engineer",
-              description:
-                "I am  Frontend Engineer with a degree in Computers and Artificial Intelligence, plus one year of startup experience. Specializing in React and Next.js, I create responsive, high-performance web applications that drive user engagement and business growth. what I offer: - Expertise in React, Next.js, and modern front-end technologies - Rapid development of user-friendly, scalable web solutions - Ability to balance technical excellence with business objectives - Collaborative approach, thriving in fast-paced environments Committed to delivering innovative web experiences that exceed expectations and propel digital success.",
-            }),
+            __html: JSON.stringify(jsonld),
           }}
         />
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_GTAG as string} />
