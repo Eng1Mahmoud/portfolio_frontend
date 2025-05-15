88278720b2fbae941041d35fa80deedf37bd9efa
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-05-15 15:50:55 +0300

    feat(layout): add structured data script for SEO optimization
    This commit introduces a structured data script in the layout component to enhance SEO by providing detailed information about the author. The script includes metadata such as name, job title, social profiles, and a professional description, which helps search engines better understand and index the content.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,24 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={`${mainFont.variable}`}>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              name: "Mahmoud Mohamed",
+              url: "https://dev-mahmoud-portfolio.vercel.app/",
+              sameAs: [
+                "https://github.com/Eng1Mahmoud",
+                "https://www.linkedin.com/in/Mahmoud-Mohamed-Abdel-Aal",
+              ],
+              jobTitle: "Frontend Engineer",
+              description:
+                "I am  Frontend Engineer with a degree in Computers and Artificial Intelligence, plus one year of startup experience. Specializing in React and Next.js, I create responsive, high-performance web applications that drive user engagement and business growth. what I offer: - Expertise in React, Next.js, and modern front-end technologies - Rapid development of user-friendly, scalable web solutions - Ability to balance technical excellence with business objectives - Collaborative approach, thriving in fast-paced environments Committed to delivering innovative web experiences that exceed expectations and propel digital success.",
+            }),
+          }}
+        />
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_GTAG as string} />
         <ToastContainer
           position="top-right"
