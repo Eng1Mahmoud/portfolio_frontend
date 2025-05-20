8c25226c6d044395923bb61f1a331ed9493c418e
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-05-20 16:21:39 +0300

    add manifest file for PWA

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Mahmoud Mohammed",
+    short_name: "Dev Mahmoud",
+    description: "Portfolio of Mahmoud Mohammed",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icons/icon192*192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icons/icon512*512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
