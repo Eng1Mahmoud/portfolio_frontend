102dfea86b2561c85441c36257069024bbcee861
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-05-20 16:26:37 +0300

    fix image path

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icons/icon192*192.png",
+        src: "/icons/icon192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon512*512.png",
+        src: "/icons/icon512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
